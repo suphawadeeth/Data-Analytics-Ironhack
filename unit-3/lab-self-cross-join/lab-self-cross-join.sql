0055d2f06469399f4c5186f44293e120d0fcbc2e
suphawadeeth <bunnymusicc@gmail.com>
2023-03-22 14:16:51 +0100

    modified unit-3/lab-self-cross-join/lab-self-cross-join.sql

--- unit-3/lab-self-cross-join/lab-self-cross-join.sql
+++ unit-3/lab-self-cross-join/lab-self-cross-join.sql
@@ -28,9 +28,7 @@ ORDER BY R1.customer_id, R2.customer_id, I.film_id, num_of_rents DESC
 
 
 # Get all possible pairs of actors and films.
-SELECT A.actor_id, 
-	CONCAT(A.first_name, " ", A.last_name) AS actor_name,
-    F.title
+SELECT A.actor_id, CONCAT(A.first_name, " ", A.last_name) AS actor_name, F.title
 FROM actor A
 CROSS JOIN film F
 ORDER BY A.actor_id, F.title;
